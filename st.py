eaa381b745522c55c4030daca95a681cbf7c6b74
Secla_Panda <61236755+SeclaPanda@users.noreply.github.com>
2022-12-24 16:35:44 +0300

    Make a repo for dke

---
+++ st.py
@@ -0,0 +1,32 @@
+import io
+import streamlit as st
+import pandas as pd
+import numpy as np
+import sqlite3 as lite
+
+def read_sqlite_table():
+
+    conn = lite.connect('db.sqlite3')
+    cur = conn.cursor()
+
+    def get_posts():
+        cur.execute("SELECT * FROM Заказ_клиента")
+        print(cur.fetchall())
+        df = pd.read_sql_query("SELECT * FROM Заказ_клиента", conn)
+        st.table(data=df)
+
+    get_posts()
+    
+
+st.title('Интернет-магазин продуктов')
+
+st.sidebar.title('Выберите нужную опцию')
+
+st.button('Просмотр баз данных', on_click=read_sqlite_table)
+st.button('Создать заказ клиента')
+st.button('Редактировать заказ клиента')
+st.button('Создать отчет по продажам')
+st.button('Создать запрос на поставку')
+st.button('Создать отчет по закупкам')
+st.button('Выход')
+#view.on_click()
