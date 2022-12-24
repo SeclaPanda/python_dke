014acb781d6f89760c26beab840d1528ebe73563
Secla_Panda <61236755+SeclaPanda@users.noreply.github.com>
2022-12-24 17:32:07 +0300

    upd

--- st.py
+++ st.py
@@ -19,9 +19,9 @@ def read_sqlite_table():
 st.title('Интернет-магазин продуктов')
 
 st.sidebar.title('Выберите нужную опцию')
-
+link1 = "https://mariaaltisheva-dke-app-p70owt.streamlit.app/Просмотр_баз_данных"
 st.button('Просмотр баз данных', on_click=read_sqlite_table)
-st.button('Создать заказ клиента')
+st.button(f'<a target="_blank" href="{link1}">Hyperlink in Streamlit dataframe</a>',)
 st.button('Редактировать заказ клиента')
 st.button('Создать отчет по продажам')
 st.button('Создать запрос на поставку')
